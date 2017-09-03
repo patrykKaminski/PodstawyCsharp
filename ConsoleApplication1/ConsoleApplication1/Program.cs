acf0b68869cf4bd643383bb32c3e7a998f30a91b
Patryk Kamiński <patkamin92@gmail.com>
2017-09-03 14:19:53 +0200

    cw2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    class przedzial
     {
         static void Main(string[] args)
         {
+            int a, b;
+            Console.WriteLine("Podaj minimalną wartość");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("podaj wartość maksymalną");
+            b = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("losowa suma to " + suma.add(a, b));
+
+            Console.ReadLine();
         }
     }
+
+    class suma
+    {
+        public static int add(int a, int b)
+        {
+            Random los = new Random();
+            int al = los.Next(a, b);
+            int bl = los.Next(a, b);
+            return al + bl;
+
+        }
+        
+    }
+    
 }
